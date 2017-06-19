9f155cfa4f5d84add97d676c819411d39ed74a20
EVgates <eviegates@yahoo.com>
2017-06-19 11:27:15 -0500

    Update proj01.py

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -8,3 +8,12 @@
 # If you complete extensions, describe your extensions here!
 
 
+print "Hello User!"
+x = raw_input("What is your name?:")
+y = raw_input("How old are you?:")
+h = raw_input("Please enter the year:")
+d = raw_input("Have you had your birthday this year yet?")
+if d==no or No or NO: print (x), ", you will turn 100 in the year" ,(z - 1), "."
+elif d==yes or Yes or YES: print (x), ", you will turn 100 in the year" ,(z), "."
+else print "The above question is a yes or no question only."
+z = 100-int(y)+int(h)
