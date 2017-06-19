68b129f5184ec41a325d0a8c978222bc4f027e35
EVgates <eviegates@yahoo.com>
2017-06-19 11:25:55 -0500

    Add files via upload

---
+++ proj01.py
@@ -0,0 +1,19 @@
+# Name:
+# Date:
+
+# proj01: A Simple Program
+# This program asks the user for his/her name and age.
+# Then, it prints a sentence that says when the user will turn 100.
+
+# If you complete extensions, describe your extensions here!
+
+
+print "Hello User!"
+x = raw_input("What is your name?:")
+y = raw_input("How old are you?:")
+h = raw_input("Please enter the year:")
+d = raw_input("Have you had your birthday this year yet?")
+if d==no or No or NO: print (x), ", you will turn 100 in the year" ,(z - 1), "."
+elif d==yes or Yes or YES: print (x), ", you will turn 100 in the year" ,(z), "."
+else print "The above question is a yes or no question only."
+z = 100-int(y)+int(h)
