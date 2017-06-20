8e8725adb5128c3057c81d642e6cd3614030ae0f
EVgates <eviegates@yahoo.com>
2017-06-20 09:50:31 -0500

    Add files via upload

--- proj02/proj02_02.py
+++ proj02/proj02_02.py
@@ -10,8 +10,19 @@ previous two numbers in the sequence. The sequence looks like this:
 1, 1, 2, 3, 5, 8, 13...
 """
 amount = int(raw_input("How many numbers do you wish to generate?"))
+x=0
+y=1
+while amount> 0:
+    z = x + y
+    print z
+    x= y
+    y= z
+    amount= amount-1
 
-while amount> 0
-    
-    amount= amount - 1
 
+for number in range (1,amount)
+    z = x+ y
+    print z
+    x= y
+    y= z
+    amount= amount + 1
