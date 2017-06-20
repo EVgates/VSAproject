87e650ae994ee6c5d68626b49ecd77e03801a971
EVgates <eviegates@yahoo.com>
2017-06-20 13:51:42 -0500

    Add files via upload

--- proj03/proj03.py
+++ proj03/proj03.py
@@ -11,5 +11,32 @@ or exactly right. Keep the game going until the user types exit.
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 """
+import random
+sprinkles= raw_input ("Do you want to play a guessing game?")
+guesses = 5
+number = random.randint(1, 99)
+while (sprinkles=="yes" or sprinkles=="Yes" or sprinkles=="YES" or sprinkles=="yeah" or sprinkles=="Yeah" or sprinkles=="YEAH" or sprinkles=="Yep" or sprinkles=="yep" or sprinkles=="YEP" or sprinkles=="totally" or sprinkles=="sure" or sprinkles=="Totally" or sprinkles=="sure" or sprinkles=="Sure" or sprinkles=="maybe" or sprinkles=="Maybe" or sprinkles==" yes" or sprinkles==" yeah" or sprinkles==" Yes" or sprinkles==" y" or sprinkles=="y" or sprinkles=="Y" or sprinkles==" yeah" or sprinkles==" Y") and guesses>0:
+    #number = random.randint(1, 99)
+    frosting= int(raw_input ("Guess a number between 1 and 99"))
 
+    if frosting==number:
+        print "Good Job! You guessed it correctly."
+        guesses= guesses+3
+        sprinkles = raw_input("Do you want to play again?")
+        number = random.randint(1, 99)
+    elif frosting>number:
+        print "Too high! Try again."
+    elif frosting<number:
+        print "Too low! Try again."
+    #else:
+        #print "Nice Try! Maybe you'll get it next time! The correct answer was",number,"."
+        #sprinkles= raw_input ("Do you want to play again?")
+    guesses = guesses -1
+if frosting==number:
+    print "Congrats, you guessed a random number correctly."
+    glitter= raw_input("Would you like to keep playing?")
+
+
+else:
+    print "Sorry, you have run out of guesses, and you failed to guess correctly. Nice try, but the correct answer was", number,"."
 
