3f327e41c135c98a6b0ba19efc1abeda2bfe0a29
EVgates <eviegates@yahoo.com>
2017-06-20 09:39:58 -0500

    Add files via upload

---
+++ proj02_02.py
@@ -0,0 +1,22 @@
+# Name:
+# Date:
+
+# proj02_02: Fibonaci Sequence
+
+"""
+Asks a user how many Fibonacci numbers to generate and generates them. The Fibonacci 
+sequence is a sequence of numbers where the next number in the sequence is the sum of the 
+previous two numbers in the sequence. The sequence looks like this: 
+1, 1, 2, 3, 5, 8, 13...
+"""
+amount = int(raw_input("How many numbers do you wish to generate?"))
+x=0
+y=1
+while amount> 0:
+    z = x + y
+    print z
+    x= y
+    y= z
+    amount= amount-1
+
+
